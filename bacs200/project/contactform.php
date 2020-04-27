84359be0d20308021277510af64d6d6f5cbfe794
samanthaewing <60017884+samanthaewing@users.noreply.github.com>
2020-04-27 15:59:09 -0600

    Fixed another error

--- bacs200/project/contactform.php
+++ bacs200/project/contactform.php
@@ -1,15 +1,16 @@
 <?php
 
-if(isset($_POST['submit']))
-    $fullname=$_POST['fullname'];
-    $email=$_POST['email'];
+if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $mailFrom=$_POST['mail'];
     $subject=$_POST['subject'];
     $message=$_POST['message'];
 
 $mailTo="ewin9631@bears.unco.edu";
-$headers="From: Summit Youth Leadership".$email;
-$txt="You have received an email from ".$fullname.".\n\n".$message;
+$headers="From: Summit Youth Leadership".$mailFrom;
+$txt="You have received an email from ".$name.".\n\n".$message;
 
 
 mail($mailTo, $subject, $txt, $headers);
 header("Location: index.php?mailsend");
+}
