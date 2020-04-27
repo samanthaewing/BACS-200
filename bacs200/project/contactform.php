f12b0d889da50d16efd7a8f81c320dc89b24d676
samanthaewing <60017884+samanthaewing@users.noreply.github.com>
2020-04-27 16:18:40 -0600

    .

--- bacs200/project/contactform.php
+++ bacs200/project/contactform.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST['submit'])){
+if(isset($_POST['submit']))
     $name=$_POST['name'];
     $mailFrom=$_POST['mail'];
     $subject=$_POST['subject'];
@@ -13,4 +13,3 @@ $txt="You have received an email from ".$name.".\n\n".$message;
 
 mail($mailTo, $subject, $txt, $headers);
 header("Location: index.php?mailsend");
-}
