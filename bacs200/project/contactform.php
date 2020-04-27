c4c133a7d4e5760a0582ccd94e4d44440f6c21c2
samanthaewing <60017884+samanthaewing@users.noreply.github.com>
2020-04-27 15:25:12 -0600

    php file for contact form

--- bacs200/project/contactform.php
+++ bacs200/project/contactform.php
@@ -1,1 +1,15 @@
+<?php
 
+if(isset($_POST['submit']))
+    $fullname=$_POST['fullname'];
+    $email=$_POST['email'];
+    $subject=$_POST['subject'];
+    $message=$_POST['message'];
+
+$mailTo="ewin9631@bears.unco.edu";
+$headers="From: Summit Youth Leadership".$mail;
+$txt="You have received an email from ".$name."./n/n".$message;
+
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
